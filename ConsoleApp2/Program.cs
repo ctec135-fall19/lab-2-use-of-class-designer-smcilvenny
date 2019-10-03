f40c3842f0ec61c58351b97bfcb77e29a98998d3
smcilvenny <s.mcilvenny@students.clark.edu>
2019-10-03 12:51:26 -0700

    Lab 2 - Doing lab1 on your own

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Sean McIlvenny and I am coding C#!");
+            Console.WriteLine("Sorry this is late but I had to miss class on Tuesday!");
+        }
+    }
+}
